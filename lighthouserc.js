88552c59563a50c7092a15d4730de7f4885791ff
damian@tripbuddi.com <damian@tripbuddi.com>
2021-01-24 13:25:06 +0700

    Turning off assertions for accessability.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,9 @@ module.exports = {
   ci: {
     assert: {
       preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:accessibility': off,
+      },
     },
     upload: {
       target: 'temporary-public-storage',
